c0dc816f79c482f8b090d5b8d1aeea366a842339
YUSUF CELIK <yusufcelik38@gmail.com>
2019-01-12 13:03:49 +0300

    Quiz Question updated

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -3,7 +3,10 @@ import React,{Component} from 'react'
 class QuizQuestionButton extends Component{
     render(){
         return(
-            <li><button>{button_text}</button></li>
+            <li>
+                <button>{this.props.button_text}
+                </button>
+                </li>
         )
     }
 }
