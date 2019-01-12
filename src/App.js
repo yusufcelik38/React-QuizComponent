a1c8300df3bc40a02098da0edc630e6fe559f239
YUSUF CELIK <yusufcelik38@gmail.com>
2019-01-12 12:36:26 +0300

    Quiz js added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Quiz from './Quiz.js'
 import './App.css'
+
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <Quiz/>
     )
   }
 }
