c3fbf253224faa2dd0d322996991dc4c72d59ec0
YUSUF CELIK <yusufcelik38@gmail.com>
2019-01-12 13:00:58 +0300

    QuizButton.js added

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react'
+
+class QuizQuestionButton extends Component{
+    render(){
+        return(
+            <li><button>{button_text}</button></li>
+        )
+    }
+}
+export default QuizQuestionButton
